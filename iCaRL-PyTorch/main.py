179c39e9df30e21713478e52d09b8b9b68793f4f
lucas.a.alcantara <lucas.a.alcantara@accenture.com>
2022-02-01 19:25:24 -0300

    reduce examplar set, update representation

--- iCaRL-PyTorch/main.py
+++ iCaRL-PyTorch/main.py
@@ -2,6 +2,8 @@ import copy
 import yaml
 import torch
 import data
+import numpy as np
+from tqdm import tqdm
 from torch import nn
 from torchvision.models import resnet34
 
@@ -12,39 +14,61 @@ def icarl_construct_exemplar_set(class_set, m, model):
 
 
 def icarl_reduce_examplar_set(m, exemplars_set):
-    pass
+    ex_set = copy.copy(exemplars_set)
+    data_shape = ex_set.data.shape[1:]
+    data_shape = (0,) + data_shape
+    ex_set.data = np.ndarray(data_shape)
+    ex_set.targets = []
+    for cls in set(exemplars_set.targets):
+        idxs = np.nonzero(np.array(exemplars_set.targets) == cls)[0].tolist()
+        ex_set.data = np.concatenate((ex_set.data,
+                                     exemplars_set.data[idxs][:m]), axis=0)
+        ex_set.targets.extend(np.array(exemplars_set.targets)[idxs][:m].tolist())
+
+    return ex_set
 
 
 def icarl_update_representation(model, dataset, exemplars_set):
-    optimizer = torch.optim.Adam(model.parameters(),
-                                 lr=0.01)
-    classification_loss = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    cls_loss_fn = torch.nn.CrossEntropyLoss()
+    disti_loss_fn = torch.nn.CrossEntropyLoss()
 
     combined_set = dataset + exemplars_set if exemplars_set else dataset
-    combined_loader = data.utils.get_dataloader(exemplars_set)
+    combined_loader = data.utils.get_dataloader(combined_set)
 
     exemplars_labels = set(exemplars_set.targets) if exemplars_set else {}
 
     old_model = copy.copy(model) 
+    old_model = old_model.to(device)
 
-    for epoch in range(70):
+    train_bar = tqdm(range(10))
+    model.train(); old_model.eval()
+    for epoch in train_bar:
         for batch, (imgs, labels) in enumerate(combined_loader):
-            imgs = imgs.to(device)
-            onehot_labels = data.utils.onehot_encoder(labels, n_classes=100)
+            model.zero_grad()
+            imgs, labels = imgs.to(device), labels.to(device)
+            #onehot_labels = data.utils.onehot_encoder(labels, n_classes=100)
             exemplars_idxs = [idx for idx, label in enumerate(labels) if label.item() in exemplars_labels]
-            previous_model_output = old_model(imgs[exemplars_idxs])
-            breakpoint()
+            with torch.no_grad():
+                previous_model_output = old_model(imgs[exemplars_idxs])
             current_model_output = model(imgs)
             
-    data_loader = data.utils.get_dataloader(combined_set)
-    pass
-
+            cls_loss_value = cls_loss_fn(current_model_output, labels)
+            dist_loss_value = disti_loss_fn(previous_model_output,
+                                            current_model_output[exemplars_idxs])
+            total_loss_value = cls_loss_value + dist_loss_value
+            total_loss_value.backward()
+            optimizer.step()
+            
 
 def icarl_incremental_train(model, dataset, k, exemplars_set):
     model = icarl_update_representation(model, dataset, exemplars_set)
     #data_loader = data.utils.get_dataloader(dataset)
-    m = k//len(exemplars_set)
-    pass
+    if exemplars_set:
+        m = k//len(set(exemplars_set.targets))
+        exemplars_set = icarl_reduce_examplar_set(m, exemplars_set)
+
+    return 
 
 
 def icarl_classify(img, class_exemplars):
@@ -67,7 +91,7 @@ def main(config):
     exemplars_set = None
     for task in range(config['n_tasks']):
         train_set = copy.copy(train_tasks[task])
-        icarl_incremental_train(model, train_set, 2000, train_set)
+        icarl_incremental_train(model, train_set, 500, exemplars_set)
 
 
 def load_config(file):
